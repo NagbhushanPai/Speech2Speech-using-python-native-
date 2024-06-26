d4733be8298b68e3622e21eab5a3b9a1709966bd
Nagbhushan_Pai <nagbhushan.mitmpl2022@learner.manipal.edu>
2024-06-26 22:19:42 +0530

    chore: Update README with speech recognition and translation instructions

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
-# Testcase
-This is a new readme file showing nothing for now.
-Let's see this change.
+## Speech Recognition and Translation
 
-# Mark1
+This Python script demonstrates how to use the `speech_recognition` library to recognize speech and translate it using the `googletrans` library.
 
-"adding stuff" <br> Heya
+### Prerequisites
+
+Before running the script, make sure you have the following libraries installed:
+
+- `speech_recognition`
+- `googletrans`
+- `pyttsx3`
+
+You can install these libraries using pip:
+
+```
+pip install speech_recognition googletrans pyttsx3
+```
+
+### Usage
+
+1. Run the script using the command `python mk.py`.
+2. Speak a sentence when prompted.
+3. The script will recognize your speech and translate it from English to Hindi.
+4. The translated text will be spoken out loud using the `pyttsx3` library.
+
+### Troubleshooting
+
+If you encounter any issues, make sure you have a working microphone connected to your computer and that it is properly configured.
+
+### Note
+
+The script uses the Google Translate API for translation. Make sure you have a stable internet connection for the translation to work properly.
+
--- mk.py
+++ mk.py
@@ -1,18 +1,88 @@
-print("Hello guys")
-ask=input(str("Hello, what is your name"))
-print("Hey ",ask," nice to meet you")
+import speech_recognition as spr
+import googletrans
+from googletrans import Translator
+import pyttsx3
 
-print("Checking this message")
-
-print("HEYA MAN")
-age = int(input("How old are you? "))
-print("You are", age, "years old.")
-print("You are", age*12, "months old.")
-#program to calculate fibonacci series
-import tensorflow as tf
-print(tf.config.list_physical_devices('GPU'))
-print("Hello guys")
-print("Just checking in...")
+# Importing necessary modules required
 
 
+# Creating Recogniser() class object
+recog1 = spr.Recognizer()
 
+# Creating microphone instance
+mc = spr.Microphone()
+
+
+# Here initialising the recorder with 
+# hello, whatever after that hello it
+# will recognise it.
+# Translator method for translation
+translator = Translator()
+
+# short form of english in which 
+# you will speak
+from_lang = 'en'
+
+# In which we want to convert, short 
+# form of hindi
+to_lang = 'hi'
+
+with mc as source:
+    
+    print("Speak a stentence...")
+    recog1.adjust_for_ambient_noise(source, duration=0.2)
+    
+    # Storing the speech into audio variable
+    audio = recog1.listen(source)
+    
+    # Using recognize.google() method to
+    # convert audio into text
+    get_sentence = recog1.recognize_google(audio)
+
+    # Using try and except block to improve
+    # its efficiency.
+    try:
+        
+        # Printing Speech which need to 
+        # be translated.
+        print("Phrase to be Translated :" + get_sentence)
+
+        text_to_translate = translator.translate(get_sentence, src=from_lang, dest=to_lang)
+
+        engine = pyttsx3.init()
+        engine.say(get_sentence)
+        engine.say(text_to_translate.text)
+        engine.runAndWait()
+
+    except spr.UnknownValueError:
+        print("Unable to Understand the Input")
+        
+    except spr.RequestError as e:
+        print("Unable to provide Required Output".format(e))
+
+
+'''
+        # Using translate() method which requires 
+        # three arguments, 1st the sentence which
+        # needs to be translated 2nd source language
+        # and 3rd to which we need to translate in 
+        text_to_translate = translator.translate(get_sentence, src=from_lang, dest=to_lang)
+        
+        # Storing the translated text in text 
+        # variable 
+        text = text_to_translate.text 
+
+        # Using pyttsx3 to speak the translated text
+        engine = pyttsx3.init()
+        engine.say(text)
+        engine.runAndWait()
+
+    # Here we are using except block for UnknownValue 
+    # and Request Error and printing the same to
+    # provide better service to the user.
+    except spr.UnknownValueError:
+        print("Unable to Understand the Input")
+        
+    except spr.RequestError as e:
+        print("Unable to provide Required Output".format(e))
+'''
