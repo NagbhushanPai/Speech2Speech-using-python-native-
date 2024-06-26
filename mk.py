1944f75d3a3dccc7323269c7de1ee6a461dbdcd3
Nagbhushan_Pai <nagbhushan.mitmpl2022@learner.manipal.edu>
2024-06-26 20:46:25 +0530

    feat: Add TensorFlow GPU support and print statements for debugging

--- mk.py
+++ mk.py
@@ -9,5 +9,10 @@ age = int(input("How old are you? "))
 print("You are", age, "years old.")
 print("You are", age*12, "months old.")
 #program to calculate fibonacci series
+import tensorflow as tf
+print(tf.config.list_physical_devices('GPU'))
+print("Hello guys")
+print("Just checking in...")
+
 
 
